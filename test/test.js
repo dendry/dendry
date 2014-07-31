18fc2c4b8969ebc72225d5b54d34188d62656c5c
idmillington <idmillington@googlemail.com>
2014-07-31 18:40:41 +0100

    Add Travis CI config, and changed indenty to two spaces.

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var should = require('should');
+
+describe("Choice", function() {
+  describe("#indexOf", function() {
+    it("should return -1 when the value is not present", function() {
+      [1,2,3].indexOf(5).should.equal(-1);
+    });
+  });
+});
