5b25abf98f7e083ee55f723f299fcf23e760e8b8
idmillington <idmillington@googlemail.com>
2014-08-10 23:05:18 +0100

    Added basic game compiler.

---
+++ test/test_compiler.js
@@ -0,0 +1,67 @@
+/* dendry
+ * http://github.com/idmillington/dendry
+ *
+ * MIT License
+ */
+/*jshint indent:2 */
+(function() {
+  "use strict";
+
+  var _ = require('lodash');
+  var should = require('should');
+  // Disable errors from using the should library.
+  /*jshint -W030 */
+
+  var compiler = require('../lib/compiler');
+
+  describe("game compiler", function() {
+
+    it("should compile a simple two-scene game", function(done) {
+      var info = {
+        title: "My Game",
+        author: "Jo Doe"
+      };
+      var listOfScenes = [
+        {id: "root", title:"Back to root", content: "Root content", options: {
+          options:[{id:"@foo", title:"Foo link"}]
+        }},
+        {id: "foo", title:"The Foo", content:"Foo content", options: {
+          options:[{id:"@foo", title:"Foo link"}]
+        }}
+      ];
+      compiler.compile(info, listOfScenes, function(err, game) {
+        (!!err).should.be.false;
+        game.sceneLookup.foo.title.should.equal('The Foo');
+        done();
+      });
+    });
+
+    it("should index scene tags", function(done) {
+      var info = {
+        title: "My Game",
+        author: "Jo Doe"
+      };
+      var listOfScenes = [
+        {id: "root", title:"Back to root", content: "Root content",
+         tags: ["alpha", "bravo"],
+         options: {
+          options:[{id:"@foo", title:"Foo link"}]
+        }},
+        {id: "foo", title:"The Foo", content:"Foo content",
+         tags: ["alpha", "charlie"],
+         options: {
+          options:[{id:"@foo", title:"Foo link"}]
+        }}
+      ];
+      compiler.compile(info, listOfScenes, function(err, game) {
+        (!!err).should.be.false;
+        game.tagLookup.alpha.should.eql(["root", "foo"]);
+        game.tagLookup.bravo.should.eql(["root"]);
+        game.tagLookup.charlie.should.eql(["foo"]);
+        console.log(JSON.stringify(game, null, 4));
+        done();
+      });
+    });
+
+  });
+}());
