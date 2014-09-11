7cebc2a14b54c4eeba5cb1c6309fab629aab8818
idmillington <idmillington@googlemail.com>
2014-09-11 10:12:15 +0100

    Added makefile for logo generation.

--- design/make-all-sizes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-convert -background none logo.svg logo_1024.png
-convert logo_1024.png -thumbnail 256x256 logo_256.png
-convert logo_1024.png -thumbnail 64x64 logo_64.png
-convert logo_1024.png -thumbnail 48x48 logo_48.png
-convert logo_1024.png -thumbnail 256x256 dendry.ico
