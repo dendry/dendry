f41536c78b1b5e5c60e57ef9ecc04fcce96f2045
idmillington <idmillington@googlemail.com>
2014-09-11 10:01:55 +0100

    Make largest logo generate from svg.

--- design/make-all-sizes.sh
+++ design/make-all-sizes.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+convert -background none logo.svg logo_1024.png
 convert logo_1024.png -thumbnail 256x256 logo_256.png
 convert logo_1024.png -thumbnail 64x64 logo_64.png
 convert logo_1024.png -thumbnail 48x48 logo_48.png
